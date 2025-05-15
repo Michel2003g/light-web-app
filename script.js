733f4ef3ffd0e0564e637b67792201afd18c0ffc
Michel2003g <111076051+Michel2003g@users.noreply.github.com>
2025-05-15 02:01:30 +0200

    Update script.js
    use api

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function newLampButton (index) {
+const lamps = [];
 
-    const container = document.getElementById("lamp-container")
+function getLampButton (index) {
+
+    const container = document.getElementById("lamp-container");
+
+    if (lamps[i] !== null) {
+        return lamps[i];
+    }
 
     // new element
     const element = document.createElement('div');
@@ -13,9 +19,38 @@ function newLampButton (index) {
             </div>`;
 
     container.appendChild(element);
+
+    lamps[i] = element;
+
 }
 
-for (let i = 0; i < 10; i++) {
-    console.log(i);
-    newLampButton(i);
-}
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+//     getLampButton(i);
+// }
+
+fetch('./api')  // vervang door het echte IP en endpoint van je ESP32
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();  // JSON data parsen
+  })
+  .then(data => {
+    console.log('Gekregen data:', data);
+    // hier kun je iets doen met je data, bijvoorbeeld:
+    // update je UI of verwerk de LED-waarden
+
+    const lamps = data.lamps
+
+    for (i = 0; i < lamps.lenght; i++) {
+        const lampButton = getLampButton(i);
+
+        if (lampButton.enabled) {
+            lampButton.classList.add("active");
+        } else {
+            lampButton.classList.remove("active");
+        };
+    };
+
+  });
