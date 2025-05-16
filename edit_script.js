1590c28c2f62b1468b42a630264c37620456864c
Michel2003g <111076051+Michel2003g@users.noreply.github.com>
2025-05-16 02:04:36 +0200

    Update edit_script.js

--- edit_script.js
+++ edit_script.js
@@ -102,6 +102,147 @@ function setEnabled (checkbox) {
     });
 }
 
+enabledCheckbox.addEventListener("change", (event) => {
+  setEnabled(event.target);
+});
+
+if (lamp === -1) {
+  lampTitle.textContent = "Editing All Lamps";
+
+  apiCall().then(apiData => {
+    let allThemes = "";
+
+    apiData.lamps.forEach(lamp => {
+      if (allThemes.includes(lamp.theme) == false) {
+        allThemes = allThemes + lamp.theme + " "
+      };
+    });
+
+    if (allThemes === "") {
+      allThemes = "NONE";
+    }
+
+    lampTheme.textContent = allThemes
+    enabledCheckbox.checked = false
+  });
+
+} else {
+  lampTitle.textContent = "Editing Lamp : " + `${(lamp + 1)}`;
+  
+  apiCall().then(apiData => {
+    const thisLamp = apiData.lamps[lamp];
+    lampTheme.textContent = thisLamp.theme.toUpperCase();
+    enabledCheckbox.checked = (thisLamp.enabled == "1");
+  });
+
+}function GetParameters() {
+
+  const search = location.search.split("?")[1]; /// get all text after ?
+  let result = {};
+
+  const items = search.split("&");
+
+  items.forEach(item => {
+    const data = item.split("=");
+    result[data[0]] = data[1];
+  });
+
+  return result;
+}
+
+async function apiCall() {
+  const response = await fetch("/api"); // of een volledige URL
+  const data = await response.json(); // als het JSON is
+
+  console.log(response)
+  console.log(data)
+
+  return data;
+}
+
+function getThemeButton (themeName) {
+
+    const container = document.getElementById("theme-container");
+
+    // new element
+    const element = document.createElement('div');
+
+    element.innerHTML = `<div class="object-button-container">
+                <button class="object-button" onClick="setTheme('${themeName}')">
+                    🔥
+                </button>
+                <p class="button-title">${themeName}</p>
+            </div>`;
+
+    const button = element.querySelector('.object-button-container');
+
+    container.appendChild(button);
+
+    return button;
+
+}
+
+const themeData = [
+  {
+    theme : "Static",
+    settings : [
+      "Color1"
+    ],
+  },
+  {
+    theme : "Fire",
+    settings : [
+      "Color2"
+    ],
+  },
+  {
+    theme : "Breathe",
+    settings : [
+      "Color1",
+      "Speed"
+    ],
+  },
+  {
+    theme : "Sparkle",
+    settings : [
+      "Color1",
+      "Amount"
+    ],
+  },
+];
+
+themeData.forEach(data => {
+  getThemeButton(data.theme);
+});
+
+const parameters = GetParameters();
+
+const lampTitle = document.getElementById("page-title");
+const lampTheme = document.getElementById("current-theme");
+const enabledCheckbox = document.getElementById("enabled-checkbox");
+
+const lamp = Number(parameters["lamp"]);
+
+function setTheme (theme) {
+  fetch(`/set?type=theme&value=${theme}&lamp=${lamp}`)
+  .then(response => response.text()) // of .json() als je JSON terugstuurt
+    .then(data => {
+      console.log(data);
+    });
+}
+
+function setEnabled (checkbox) {
+  fetch(`/set?type=enabled&value=${checkbox.checked}&lamp=${lamp}`)
+  .then(response => response.text()) // of .json() als je JSON terugstuurt
+    .then(data => {
+      console.log(data);
+    });
+}
+
+enabledCheckbox.addEventListener("change", (event) => {
+  setEnabled(event.target);
+});
+
 if (lamp === -1) {
   lampTitle.textContent = "Editing All Lamps";
 
